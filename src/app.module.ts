d0b73cf2b24369a03ce4a2c3904664d5cc70f117
Alexis Vivier <alexisvivier@macbook-air-de-alexis.home>
2023-07-31 12:11:06 +0200

    Add Config and .dev.env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {YogaDriver, YogaDriverConfig} from "@graphql-yoga/nestjs";
 import {GraphQLModule} from "@nestjs/graphql";
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+      isGlobal: true,
+    }),
     GraphQLModule.forRoot<YogaDriverConfig>({
       driver: YogaDriver
     })
